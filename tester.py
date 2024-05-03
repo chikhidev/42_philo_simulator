93831622f94e5e59f92bae4d855ad29924cf8498
chikhidev <chikhi.dev@gmail.com>
2024-05-03 11:19:59 +0100

    push

--- tester.py
+++ tester.py
@@ -56,30 +56,33 @@ def testCase(numOfPhilosophers, timeToDie, timeToEat, timeToSleep, numOfMeals):
     lines = res.splitlines()
 
     for line in lines:
-        split = line.split()
-        time = int(split[0])
-        name = int(split[1])
-        status = ""
-        for i in range(2, len(split)):
-            status += split[i] + " "
-        philosophers[name-1]['status'] = status
+        try:
+            split = line.split()
+            time = int(split[0])
+            name = int(split[1])
+            status = ""
+            for i in range(2, len(split)):
+                status += split[i] + " "
+            philosophers[name-1]['status'] = status
 
-        if ((philosophers[name-1]['status'] == "has taken a fork ") and philosophers[name-1]['nforks'] < 2):
-            philosophers[name-1]['nforks'] += 1
-        else:
-            philosophers[name-1]['nforks'] = 0
+            if ((philosophers[name-1]['status'] == "has taken a fork ") and philosophers[name-1]['nforks'] < 2):
+                philosophers[name-1]['nforks'] += 1
+            else:
+                philosophers[name-1]['nforks'] = 0
 
-        philosophers[name-1]['time'] = time
-        philosophers[name-1]['name'] = name
-        system("clear")
-        drawTable(philosophers)
-        print("Total forks taken: " + BLUE + str(sum([philosopher['nforks'] for philosopher in philosophers])) + "\n" + END)
-        print("current action:\n" + GREEN + line + END)
-        print("previous action:\n" + MAGENTA + prev_line + END) if prev_line != None else None
-        prev_line = line
-        print("Step " + str(step) + " of " + str(len(lines)))
-        step += 1
-        sleep(speed)
+            philosophers[name-1]['time'] = time
+            philosophers[name-1]['name'] = name
+            system("clear")
+            drawTable(philosophers)
+            print("Total forks taken: " + BLUE + str(sum([philosopher['nforks'] for philosopher in philosophers])) + "\n" + END)
+            print("current action:\n" + GREEN + line + END)
+            print("previous action:\n" + MAGENTA + prev_line + END) if prev_line != None else None
+            prev_line = line
+            print("Step " + str(step) + " of " + str(len(lines)))
+            step += 1
+            sleep(speed)
+        except Exception as e:
+            print(e)
 
     
 
@@ -94,29 +97,33 @@ if __name__ == '__main__':
         print(BOLDRED + "Error: Makefile not found!" + END)
         exit(1)
 
-    prompt = input("Enter the prompt: ")
-    inputs = prompt.split()
-    numOfPhilosophers = int(inputs[0])
-    timeToDie = int(inputs[1])
-    timeToEat = int(inputs[2])
-    timeToSleep = int(inputs[3])
+    prompt = ""
     try:
-        nm = inputs[4]
-    except:
-        nm = ""
+        while (prompt ==  ""):
+            prompt = input("Enter the prompt: ")
+        inputs = prompt.split()
+        numOfPhilosophers = int(inputs[0])
+        timeToDie = int(inputs[1])
+        timeToEat = int(inputs[2])
+        timeToSleep = int(inputs[3])
+        try:
+            nm = inputs[4]
+        except:
+            nm = ""
+        if (nm == ""):
+            numOfMeals = 50
+            print("simulating 50 meals as a limit...")
+        else:
+            numOfMeals = int(nm)
 
-    if (nm == ""):
-        numOfMeals = 50
-        print("simulating 50 meals as a limit...")
-    else:
-        numOfMeals = int(nm)
+        speed_input = input("Enter the speed of the simulation (0.1 - 1) <0.8> by default: ")
+        if (speed_input == ""):
+            speed = 0.8
+        else:
+            speed = float(speed_input)
 
-    speed_input = input("Enter the speed of the simulation (0.1 - 1) <0.8> by default: ")
-    if (speed_input == ""):
-        speed = 0.8
-    else:
-        speed = float(speed_input)
+        testCase(numOfPhilosophers, timeToDie, timeToEat, timeToSleep, numOfMeals)
 
-    testCase(numOfPhilosophers, timeToDie, timeToEat, timeToSleep, numOfMeals)
-
-    print(BOLDGREEN + "Simulation ended!" + END)
+        print(BOLDGREEN + "Simulation ended!" + END)
+    except Exception as e:
+        print("Wrong prompt!")
